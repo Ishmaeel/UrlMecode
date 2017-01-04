ec81acfb29f8a6ef7beaf05912be061300a438eb
Ishmaeel <Ishmaeel@users.noreply.github.com>
2017-01-04 14:33:35 +0300

    Go forth and multiply!

---
+++ src/Mecoder.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace UrlMecode
+{
+    internal class Mecoder : INotifyPropertyChanged
+    {
+        private string _Input;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string DecodedOutput { get; set; }
+
+        public string EncodedOutput { get; set; }
+
+        public string Input
+        {
+            get { return _Input; }
+            set
+            {
+                _Input = value;
+
+                EncodedOutput = System.Web.HttpUtility.UrlEncode(_Input);
+                RaiseChangedEvent("EncodedOutput");
+
+                DecodedOutput = System.Web.HttpUtility.UrlDecode(_Input);
+                RaiseChangedEvent("DecodedOutput");
+            }
+        }
+
+        private void RaiseChangedEvent(string prop)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
